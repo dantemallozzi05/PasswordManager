4423b6a2e20f4b7abfcf23cf13452d377d2a2dff
dantemallozzi05 <dante.mallozzi@hotmail.com>
2025-10-05 22:49:07 -0400

    Added Vault class implementation

--- src/Vault.cpp
+++ src/Vault.cpp
@@ -1,2 +1,44 @@
 #include "Vault.h"
+#include "crypto.h"
+#include <nlohmann/json.hpp>
+#include <fstream>
 
+Vault::Vault(const std::string& path, const std::string& masterPassword) : filePath(path) {
+	Crypto::KdfParams kdf;
+	kdf.salt = Crypto::randomBytes(crypto_pwhash_SALTBYTES);
+	Crypto::deriveKey(masterPassword, kdf, key);
+	nonce = Crypto::randomBytes(crypto_aead_schacha20poly1305_ietf_NPUBBYTES);
+
+}
+
+bool Vault::addEntry(const Entry& entry) {
+	entries.push_back(entry);
+	return true;
+}
+
+bool Vault::save() {
+	nlohmann::json j = entries;
+	std::string plaintext = j.dump();
+
+	std::string encrypted;
+	if (!Crypto::encrypt(key, nonce, plaintext, encrypted))
+		return false;
+
+	std::ofstream ofs(filePath, std::ios::binary);
+	ofs << encrypted;
+	return true;
+}
+
+bool Vault::load() {
+	std::ifstream ifs(filePath, std::ios::binary);
+	if (!ifs.is_open()) return false;
+
+	std::string encrypted((std::istreambuf_iterator<char>(ifs)), {});
+	std::string plaintext;
+	if (!Crypto::decrypt(key, nonce, encrypted, plaintext))
+		return false;
+}
+
+std::vector<Entry> Vault::getEntries() const {
+	return entries;
+}
--- src/Vault.h
+++ src/Vault.h
@@ -1,12 +1,28 @@
 #pragma once
 #include <string>
+#include <vector>
+#include "crypto.h"
+
+struct Entry {
+	std::string site;
+	std::string username;
+	std::string password;
+};
 
 class Vault {
 public:
 	Vault();
-	void addEntry(const std::string& site);
+	void addEntry(const std::string& filePath, const std::string& masterPassword);
+	bool addEntry(const Entry& entry);
+	bool load();
+	bool save();
+	std::vector<Entry> getEntries() const;
 
 private:
+	std::string filePath;
+	std::vector<Entry> entries;
+	std::vector<unsigned char> key;
+	std::vector<unsigned char> nonce;
 	int numEntries;
 
 };
